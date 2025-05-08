7efe6c8ed835eb942fb2884b143865f8d2f7c44b
unknown <danosqui20@gmail.com>
2025-05-08 14:07:28 -0300

    no logre que escriba en el archivo

--- chatbot2/chat.py
+++ chatbot2/chat.py
@@ -1,13 +1,29 @@
 import csv
 
-print("hola")
+print("Hola! Soy un chatbot de informacion sobre pokemon o no se")
 
-pregunta = input("pone tu pregunta: ")
+print("¿En que puedo ayudarte? Si queres salir, solo di 'salir'")
 
-with open('preguntas.csv', mode ='r') as file:    
+pregunta = input()
 
-       csvFile = csv.DictReader(file)
 
-       for filas in csvFile:
-            if (pregunta == filas["pregunta"]):
-                  print(filas["respuesta"])
+while pregunta != "salir":
+      with open('preguntas.csv', mode ='r') as archivo:
+            csvFile = csv.DictReader(archivo)
+
+            preguntaEsta = False
+            for filas in csvFile:
+                  if (pregunta == filas["pregunta"]):
+                        print(filas["respuesta"])
+                        preguntaEsta = True
+            if preguntaEsta == False:
+                  print("No tengo la respuesta a esa pregunta! Me la anoto para la proxima vez.")
+                  # writer = csv.writer(archivo)
+                  # writer.writerow([pregunta, "No tengo la respuesta a esa pregunta!"])
+
+
+            print("¿En que puedo ayudarte? Si queres salir, solo di 'salir'")
+            pregunta = input()
+           
+
+print("Gracias por usar el chatbot! Hasta luego!")
